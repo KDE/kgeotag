975dae4202f59b0b5b872bed2a68afbb19e2e5ac
Johannes Zarl-Zierl <johannes@zarl-zierl.at>
2020-11-07 01:23:55 +0100

    Reuse compatibility

--- cmake/GitDescription.cmake
+++ cmake/GitDescription.cmake
@@ -1,10 +1,6 @@
-# Copyright 2020 Tobias Leupold <tobias.leupold@gmx.de>
-# Copyright 2012-2020 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# This file is under the public domain and can be reused without restrictions.
+# SPDX-FileCopyrightText: 2012-2020 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
+# SPDX-FileCopyrightText: 2020 Tobias Leupold <tobias.leupold@gmx.de>
+# SPDX-License-Identifier: BSD-2-Clause
 
 # - GIT_GET_DESCRIPTION ( description_variable [GIT_ARGS arg1 ...] [SEND_ERROR] )
 # This macro calls git describe to obtain a concise description of the current
