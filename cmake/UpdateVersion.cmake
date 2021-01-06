a509462a12ff23d810e892ad0dc1c0e9410fbcc4
Tobias Leupold <tobias.leupold@gmx.de>
2021-01-06 12:04:48 +0100

    Generate version.h from the project() call in CMakeLists.txt ...
    ... if we can't get the version from git (e. g. when compiling a release
    tarball)

--- cmake/UpdateVersion.cmake
+++ cmake/UpdateVersion.cmake
@@ -1,5 +1,5 @@
+# SPDX-FileCopyrightText: 2018-2021 Tobias Leupold <tobias.leupold@gmx.de>
 # SPDX-FileCopyrightText: 2012-2018 Johannes Zarl-Zierl <johannes@zarl-zierl.at>
-# SPDX-FileCopyrightText: 2018-2020 Tobias Leupold <tobias.leupold@gmx.de>
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,35 +9,30 @@ if (NOT DEFINED BASE_DIR)
 endif()
 
 # Version header locations
-set(VERSION_H_RELEASE   ${BASE_DIR}/src/version.h)
 set(VERSION_H_IN        ${BASE_DIR}/src/version.h.in)
 set(VERSION_H_CURRENT   ${CMAKE_BINARY_DIR}/version.h.current)
 set(VERSION_H_GENERATED ${CMAKE_BINARY_DIR}/version.h)
 
-if (EXISTS ${VERSION_H_RELEASE})
-    # A ready to use version.h has been found, so we're compiling a release tarball
-    message(STATUS "Using the released version.h")
-else()
+if (EXISTS "${BASE_DIR}/.git" AND EXISTS "${VERSION_H_IN}")
     # We're not working on a release tarball and have to generate a version.h
-    if (EXISTS "${BASE_DIR}/.git" AND EXISTS "${VERSION_H_IN}")
-        include(${CMAKE_CURRENT_LIST_DIR}/GitDescription.cmake)
-        git_get_description(VERSION GIT_ARGS)
-        if (NOT VERSION)
-            set(VERSION "unknown")
-        endif()
-
-        message(STATUS "Updating version information to ${VERSION}")
-        # write version info to a temporary file
-        configure_file(${VERSION_H_IN} ${VERSION_H_CURRENT})
-        # update info if changed
-        execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                                ${VERSION_H_CURRENT} ${VERSION_H_GENERATED})
-        # make sure info doesn't get stale
-        file(REMOVE ${VERSION_H_CURRENT})
-
-    else()
-        message(SEND_ERROR "The generated file \"version.h\" does not exist!")
-        message(AUTHOR_WARNING "When creating a release tarball, be sure to include a version.h "
-                               "file including the correct version number!")
+    message(STATUS "Generating version.h from git")
+    include(${CMAKE_CURRENT_LIST_DIR}/GitDescription.cmake)
+    git_get_description(VERSION GIT_ARGS)
+    if (NOT VERSION)
+        set(VERSION "unknown")
     endif()
+else()
+    # We're compiling a release tarball and generate version.h
+    # from the CMakeLists.txt project version
+    message(STATUS "Generating version.h from CMakeLists.txt")
+    set(VERSION "${PROJECT_VERSION}")
 endif()
+
+message(STATUS "Updating version information to ${VERSION}")
+# write version info to a temporary file
+configure_file(${VERSION_H_IN} ${VERSION_H_CURRENT})
+# update info if changed
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                        ${VERSION_H_CURRENT} ${VERSION_H_GENERATED})
+# make sure info doesn't get stale
+file(REMOVE ${VERSION_H_CURRENT})
