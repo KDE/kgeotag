e15f3cff27b81cc86fe5fd94667001933fd48e6c
Kai Uwe Broulik <kde@privat.broulik.de>
2024-12-02 17:42:22 +0100

    Set AA_ShareOpenGLContext
    Fixes web engine not working on Marble.

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
     QApplication application(argc, argv);
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
