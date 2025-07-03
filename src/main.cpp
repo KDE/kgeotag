551e744fd96eceb30202823ff63d15e52fe2eaf2
Tobias Leupold <tl@stonemx.de>
2025-07-03 10:10:08 +0200

    Removed all preprocessor Qt version checks

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,6 @@
 int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
-#endif
 
     KLocalizedString::setApplicationDomain("kgeotag");
     KCrash::initialize();
